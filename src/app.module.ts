5b070fbaaf6fea6d10d83060b775caa2423e0844
Volodymyr Nerovnia <nerv@i.ua>
2023-08-08 21:43:01 +0300

    Setup Nest.JS

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.development.env', '.production.env'],
+  }), 
+  MongooseModule.forRoot(process.env.MONGO_URI), UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
