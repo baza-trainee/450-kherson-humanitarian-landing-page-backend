fc7f7cda2e846b2fe97a8a561b351d32fd183ce2
Volodymyr Nerovnia <nerv@i.ua>
2023-08-11 23:25:14 +0300

    Enhance syntax in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: `.${process.env.NODE_ENV}.env`,
-  }), 
-  MongooseModule.forRoot(process.env.MONGO_URI), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
