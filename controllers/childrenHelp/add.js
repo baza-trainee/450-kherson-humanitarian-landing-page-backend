9fb5f53f8ebdf6b942f9fc94f232d1ff28446786
Volodymyr Nerovnia <nerv@i.ua>
2023-08-22 02:16:45 +0300

    Init Express

--- controllers/childrenHelp/add.js
+++
@@ -1,9 +0,0 @@
-const { ChildrenHelp } = require('../../models');
-const add = async (req, res) => {
-  // const { _id: owner } = req.user;
-  const result = await ChildrenHelp.create(req.body);
-
-  res.status(201).json(result);
-};
-
-module.exports = add;
--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const auth = require('./auth');
-const contacts = require('./contacts');
-const idpHelp = require('./idpHelp');
-const disHelp = require('./disHelp');
-const childrenHelp = require('./childrenHelp');
-const mainCollectionList = require('./mainCollectionList');
-
-module.exports = { auth, contacts, idpHelp, disHelp, childrenHelp, mainCollectionList };
