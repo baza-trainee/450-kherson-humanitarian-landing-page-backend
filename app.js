a66f9354c9c790fa7719ef1bf23681e5e18448f5
Oleksandr Pavlishchev <velogitara@gmail.com>
2023-08-31 11:40:27 +0300

    add cors options

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const corsOption = require('./config/api/v1/corsOption');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 var api1Router = require('./routes/api1');
 
-const helmet = require('helmet')
+const helmet = require('helmet');
 
 require('dotenv').config();
 
@@ -23,6 +26,7 @@ app.disable('x-powered-by');
 //app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -43,21 +47,21 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message: message });
 });
 
-const startExpress= async () => {
-  app.use(function(err, req, res, next) {
+const startExpress = async () => {
+  app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
     // render the error page
     res.status(err.status || 500);
     res.render('error');
   });
-}
+};
 
 const startMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB сервер запущений');
   } catch (err) {
     console.log('Помилка при запуску MongoDB серверу');
@@ -65,7 +69,7 @@ const startMongoDB = async () => {
       startMongoDB();
     }, 5000);
   }
-}
+};
 const startServer = async () => {
   try {
     await startMongoDB();
@@ -74,7 +78,7 @@ const startServer = async () => {
   } catch (err) {
     console.log('Помилка при запуску сервера');
   }
-}
+};
 
 startServer();
 
