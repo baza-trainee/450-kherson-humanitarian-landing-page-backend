3d4db97d7f07624dbbf41b1a6480a8efa805c161
Volodymyr Nerovnia <nerv@i.ua>
2023-09-03 17:09:16 +0300

    Enhanced hero middleware Created config for api components Created simpleIssueValidator

--- middleware/api/1/hero.js
+++ middleware/api/1/hero.js
@@ -1,11 +1,46 @@
+/*
+ * Copyright (c) 2023 Volodymyr Nerovnia
+ * SPDX-License-Identifier: MIT
+ */
+
+const { isImageValid, isColorValid, isTextValid } = require("../../../utils/helpers/api/simpleIssueValidator");
+
 /**
  * Check if Hero object is valid .
  */
 
 function isValidHero(req, res, next) {
   //console.log(req.headers);
-  console.log(req.body);
-  next();
+  //console.log(req.body);
+  try {
+    const { View, Title, Subtitle} = req.body;
+
+    // check View
+    const isView = (isImageValid(View.picture, 500) && 
+                (isColorValid(View.bgColorStart)) && 
+                (isColorValid(View.bgColorEnd)));
+  
+    // check Title
+    const isTitle = (isTextValid(Title.text, minLength, maxLength) && 
+                (isColorValid(Title.colorStart)) && 
+                (isColorValid(Title.colorMiddle)) && 
+                (isColorValid(Title.colorEnd)));
+  
+  
+    // check Subtitle
+    const isSubtitle = (isTextValid(Subtitle.text, minLength, maxLength) && 
+                (isColorValid(Subtitle.colorStart)) && 
+                (isColorValid(Subtitle.colorMiddle)) && 
+                (isColorValid(Subtitle.colorEnd)));
+
+    if( isView && isTitle && isSubtitle) {
+      next();
+    } else {
+      return res.status(406).json({message: "Помилка валідації даних"})
+    }
+  } catch (err) {
+    return res.status(406).json({message: "Помилка валідації даних"})
+  }
   /*
   if (req.method === "OPTIONS") {
   }
