7a97fb895e3710e35d65bdf7b4858b799d5628f6
Volodymyr Nerovnia <nerv@i.ua>
2023-09-08 03:33:03 +0300

    Add resource path Added example.env Cleanes public directory

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Volodymyr Nerovnia <nerv@i.ua>, 
+ * Copyright (c) 2023 Volodymyr Nerovnia <nerv@i.ua>,
  *                    Oleksandr Pavlishchev <velogitara@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -23,40 +23,40 @@
  * SPDX-License-Identifier: MIT
  */
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
-const config = require('./config/app');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const config = require("./config/app");
 
-var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-var api1Router = require('./routes/api1');
+var indexRouter = require("./routes/index");
+var authRouter = require("./routes/auth");
+var api1Router = require("./routes/api1");
 
-const helmet = require('helmet')
+const helmet = require("helmet");
 
-require('dotenv').config();
+require("dotenv").config();
 
 var app = express();
 
 app.use(helmet());
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use("/resources", express.static(path.join(__dirname, "public")));
 //app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/api/v1', api1Router);
+app.use("/auth", authRouter);
+app.use("/api/v1", api1Router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -64,43 +64,43 @@ app.use(function (req, res, next) {
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
 
   res.status(status).json({ message: message });
 });
 
-const startExpress= async () => {
-  app.use(function(err, req, res, next) {
+const startExpress = async () => {
+  app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
   });
-}
+};
 
 const startMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log('MongoDB сервер запущений');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB сервер запущений");
   } catch (err) {
-    console.log('Помилка при запуску MongoDB серверу');
+    console.log("Помилка при запуску MongoDB серверу");
     setTimeout(() => {
       startMongoDB();
     }, config.server.MongoDB.restartSec);
   }
-}
+};
 const startServer = async () => {
   try {
     await startMongoDB();
     startExpress();
-    console.log('API Server чекає на отримання запитів...');
+    console.log("API Server чекає на отримання запитів...");
   } catch (err) {
-    console.log('Помилка при запуску сервера');
+    console.log("Помилка при запуску сервера");
   }
-}
+};
 
 startServer();
 
