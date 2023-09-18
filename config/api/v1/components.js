50345d038dcdcfc1ee0030254258a5e9d5500b37
Volodymyr Nerovnia <nerv@i.ua>
2023-09-18 03:17:39 +0300

    Changed limites for components

--- config/api/v1/components.js
+++ config/api/v1/components.js
@@ -7,179 +7,179 @@ module.exports = {
   hero: {
     View: {
       picture: {
-        maxSizeKb: 500
-      }
+        maxSizeKb: 500,
+      },
     },
     title: {
       text: {
         minLength: 66,
-        maxLength: 90
-      }
+        maxLength: 90,
+      },
     },
     subtitle: {
       text: {
         minLength: 13,
-        maxLength: 40
-      }
-    }
+        maxLength: 40,
+      },
+    },
   },
   fund: {
-    picture:{
-      maxSizeKb: 500
-    }
+    picture: {
+      maxSizeKb: 500,
+    },
   },
   team: {
-    picture:{
-      maxSizeKb: 500
+    picture: {
+      maxSizeKb: 500,
     },
     title: {
       minLength: 12,
-      maxLength: 45
+      maxLength: 45,
     },
     text: {
       minLength: 243,
-      maxLength: 900
-    }
+      maxLength: 900,
+    },
   },
   history: {
-    picture:{
-      maxSizeKb: 500
+    picture: {
+      maxSizeKb: 500,
     },
     title: {
       minLength: 12,
-      maxLength: 45
+      maxLength: 45,
     },
     text: {
       minLength: 595,
-      maxLength: 963
-    }
+      maxLength: 963,
+    },
   },
   achievement: {
     issuedHumanitarianKits: {
       min: 1,
-      max: 9999999999
+      max: 9999999999,
     },
     receivedHumanitarianAid: {
       min: 1,
-      max: 9999999999
+      max: 9999999999,
     },
     sumDonats: {
       min: 100000,
-      max: 999999999999
+      max: 999999999999,
     },
     infoAtDate: {
-      minDate: '08/01/2023',
-      maxDate: new Date()
-    }
+      minDate: "08/01/2023",
+      maxDate: new Date(),
+    },
   },
   issuepoint: {
     locationDeliveryPoint: {
       minLength: 35,
-      maxLength: 78
+      maxLength: 78,
     },
     geolocation: {
       minLength: 400,
-      maxLength: 630
-    }
+      maxLength: 630,
+    },
   },
   activity: {
-    picture:{
-      maxSizeKb: 500
+    picture: {
+      maxSizeKb: 500,
     },
   },
   projects: {
     pictures: {
-      maxSizeKb: 500
+      maxSizeKb: 500,
     },
     title: {
       minLength: 56,
-      maxLength: 73
+      maxLength: 73,
     },
     subTitle: {
       minLength: 82,
-      maxLength: 125
+      maxLength: 125,
     },
     text: {
       minLength: 236,
-      maxLength: 503
+      maxLength: 503,
     },
     areaCompletedWorks: {
       minLength: 3,
-      maxLength: 100
+      maxLength: 100,
     },
     projectDuration: {
-      minLength: 27,
-      maxLength: 29
+      minLength: 20,
+      maxLength: 29,
     },
     projectText: {
-      minLength: 77,
-      maxLength: 248
-    }
+      minLength: 60,
+      maxLength: 248,
+    },
   },
   logos: {
-    pictures:{
-      maxSizeKb: 500
-    }
+    pictures: {
+      maxSizeKb: 500,
+    },
   },
   contacts: {
     email: {
-      minLength: 26,
-      maxLength: 33
+      minLength: 8,
+      maxLength: 33,
     },
     address: {
       minLength: 71,
-      maxLength: 108
+      maxLength: 108,
     },
     phone: {
       minLength: 9,
-      maxLength: 12
-    }
+      maxLength: 13,
+    },
   },
   documents: {
     rules: {
-      maxSizeKb: 50000
+      maxSizeMb: 50 * 1024 * 1024,
     },
     publicOfferContract: {
-      maxSizeKb: 50000
+      maxSizeMb: 50 * 1024 * 1024,
     },
     privacy: {
-      maxSizeKb: 50000
+      maxSizeMb: 50 * 1024 * 1024,
     },
     statut: {
-      maxSizeKb: 50000
+      maxSizeMb: 50 * 1024 * 1024,
     },
     reporting: {
-      maxSizeKb: 50000
-    }
+      maxSizeMb: 50 * 1024 * 1024,
+    },
   },
   congrats: {
     confirmRegistration: {
       chapterText: {
         minLength: 80,
-        maxLength: 120
-      }
-    }
+        maxLength: 120,
+      },
+    },
   },
   donats: {
     currency: {
       minLength: 3,
-      maxLength: 3
+      maxLength: 3,
     },
     recipient: {
       minLength: 40,
-      maxLength: 98
+      maxLength: 98,
     },
     IBAN: {
       minLength: 29,
-      maxLength: 29
+      maxLength: 29,
     },
     IPN: {
       minLength: 8,
-      maxLength: 10
+      maxLength: 10,
     },
     paymentPurpose: {
       minLength: 18,
-      maxLength: 40
-    }
-  }
-}
+      maxLength: 40,
+    },
+  },
+};
