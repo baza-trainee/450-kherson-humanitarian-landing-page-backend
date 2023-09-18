092a0223b123e229f477616893207248d81407a8
Volodymyr Nerovnia <nerv@i.ua>
2023-09-18 03:21:54 +0300

    Update document controller

--- controllers/api/1/documents.js
+++ controllers/api/1/documents.js
@@ -1,19 +1,56 @@
-
-
 const getDocuments = async (req, res, next) => {
   try {
-    res.status(501).json({message: 'Очікує на реалізацію'})
+    res.status(501).json({ message: "Очікує на реалізацію" });
   } catch (err) {
-    res.status(500).json({message: 'Помилка на боці серверу'})
+    res.status(500).json({ message: "Помилка на боці серверу" });
   }
-}
+};
 
-const updateDocuments = async (req, res, next) => {
+const uploadRules = async (req, res, next) => {
   try {
-    res.status(501).json({message: 'Очікує на реалізацію'})
+    res.status(501).json({ message: "Очікує на реалізацію" });
   } catch (err) {
-    res.status(500).json({message: 'Помилка на боці серверу'})
+    res.status(500).json({ message: "Помилка на боці серверу" });
   }
-}
+};
 
-module.exports = {getDocuments, updateDocuments};
+const uploadPublicOfferContract = async (req, res, next) => {
+  try {
+    res.status(501).json({ message: "Очікує на реалізацію" });
+  } catch (err) {
+    res.status(500).json({ message: "Помилка на боці серверу" });
+  }
+};
+
+const uploadPrivacy = async (req, res, next) => {
+  try {
+    res.status(501).json({ message: "Очікує на реалізацію" });
+  } catch (err) {
+    res.status(500).json({ message: "Помилка на боці серверу" });
+  }
+};
+
+const uploadStatut = async (req, res, next) => {
+  try {
+    res.status(501).json({ message: "Очікує на реалізацію" });
+  } catch (err) {
+    res.status(500).json({ message: "Помилка на боці серверу" });
+  }
+};
+
+const uploadReporting = async (req, res, next) => {
+  try {
+    res.status(501).json({ message: "Очікує на реалізацію" });
+  } catch (err) {
+    res.status(500).json({ message: "Помилка на боці серверу" });
+  }
+};
+
+module.exports = {
+  getDocuments,
+  uploadRules,
+  uploadPublicOfferContract,
+  uploadPrivacy,
+  uploadStatut,
+  uploadReporting,
+};
