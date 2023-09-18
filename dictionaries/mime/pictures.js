2aa9e4c8665d26b785c7fef2b24d3212728cc398
Volodymyr Nerovnia <nerv@i.ua>
2023-09-18 03:16:31 +0300

    Added pictures mime types dictionary

---
+++ dictionaries/mime/pictures.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023 Volodymyr Nerovnia
+ * SPDX-License-Identifier: MIT
+ */
+
+module.exports = [
+  {
+    mimeName: "image/apng",
+    extension: ".apng",
+  },
+  {
+    mimeName: "image/avif",
+    extension: ".avif",
+  },
+  {
+    mimeName: "image/gif",
+    extension: ".gif",
+  },
+  {
+    mimeName: "image/jpeg",
+    extension: ".jpg",
+  },
+  {
+    mimeName: "image/png",
+    extension: ".png",
+  },
+  {
+    mimeName: "image/svg+xml",
+    extension: ".svg",
+  },
+  {
+    mimeName: "image/webp",
+    extension: ".webp",
+  },
+];
