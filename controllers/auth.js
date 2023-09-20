915f02bf552fbe1dd3c6f4b1a0acf913a6396520
Volodymyr Nerovnia <nerv@i.ua>
2023-09-20 12:52:22 +0300

    Changed formatting controllers/auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,21 @@
-const User = require('../models/auth/User');
-const bcrypt = require('bcryptjs');
-const { validationResult } = require('express-validator');
-const jwt = require('jsonwebtoken');
-const mailService = require('../service/mail-service');
-const uuid = require('uuid');
-const { token, link } = require('../config/auth');
-const { urls } = require('../config/app');
+const User = require("../models/auth/User");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const mailService = require("../service/mail-service");
+const uuid = require("uuid");
+const { token, link } = require("../config/auth");
+const { urls } = require("../config/app");
 
 /**
  * Generator bearer tokens
  */
 
-const generateAccessToken = id => {
+const generateAccessToken = (id) => {
   const payload = { id };
-  return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: token.expiresTime });
+  return jwt.sign(payload, process.env.SECRET_KEY, {
+    expiresIn: token.expiresTime,
+  });
 };
 /**
  * Login validation
@@ -26,18 +28,18 @@ const login = async (req, res, next) => {
     await createAdminInDB();
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(400).json({ message: 'Помилка авторизації' });
+      return res.status(400).json({ message: "Помилка авторизації" });
     }
 
     const validPassword = bcrypt.compareSync(password, user.password);
     if (!validPassword) {
-      return res.status(400).json({ message: 'Помилка авторизації' });
+      return res.status(400).json({ message: "Помилка авторизації" });
     }
     responseResult.token = generateAccessToken(user._id);
     return res.status(200).json(responseResult);
   } catch (err) {
     //console.log(err);
-    res.status(400).json({ message: 'Помилка авторизації' });
+    res.status(400).json({ message: "Помилка авторизації" });
   }
 };
 
@@ -46,13 +48,16 @@ const login = async (req, res, next) => {
  */
 
 const renewPassword = async (req, res, next) => {
+  console.log(urls.APP_URL);
   try {
     const { username } = req.body;
     await createAdminInDB();
     console.log(username);
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(200).json({ message: 'Посилання для відновлення паролю відправлено' });
+      return res
+        .status(200)
+        .json({ message: "Посилання для відновлення паролю відправлено" });
     }
     const activationLink = uuid.v4();
     user.renewPasswordLink = activationLink;
@@ -63,10 +68,12 @@ const renewPassword = async (req, res, next) => {
       process.env.ADMIN_EMAIL,
       `${urls.APP_URL}/auth/renew/${activationLink}`
     );
-    return res.status(200).json({ message: 'Посилання для відновлення паролю відправлено' });
+    return res
+      .status(200)
+      .json({ message: "Посилання для відновлення паролю відправлено" });
   } catch (err) {
     //console.log(err);
-    res.status(400).json({ message: 'Помилка авторизації' });
+    res.status(400).json({ message: "Помилка авторизації" });
   }
 };
 
@@ -80,13 +87,17 @@ const renewPasswordLink = async (req, res, next) => {
     const renewPasswordLink = req.params.link;
     const user = await User.findOne({ renewPasswordLink });
     console.log(link.expiresTime);
-    if (!user || user?.renewPasswordDate.getTime() + link.expiresTime < new Date().getTime()) {
-      return res.status(400).json({ message: 'Посилання не дійсне' });
+    if (
+      !user ||
+      user?.renewPasswordDate.getTime() + link.expiresTime <
+        new Date().getTime()
+    ) {
+      return res.status(400).json({ message: "Посилання не дійсне" });
     }
     responseResult.token = generateAccessToken(user._id);
     return res.status(200).json(responseResult);
   } catch (err) {
-    res.status(400).json({ message: 'Помилка авторизації' });
+    res.status(400).json({ message: "Помилка авторизації" });
   }
 };
 
@@ -99,15 +110,15 @@ const changePassword = async (req, res, next) => {
     const responseResult = {};
     const user = await User.findOne({ _id: req.user.id });
     if (!user) {
-      return res.status(403).json({ message: 'Користувач не авторизований' });
+      return res.status(403).json({ message: "Користувач не авторизований" });
     }
     const { password } = req.body;
     const hashPassword = bcrypt.hashSync(password, 7);
     user.password = hashPassword;
     await user.save();
-    return res.status(200).json({ message: 'Пароль користувача змінено' });
+    return res.status(200).json({ message: "Пароль користувача змінено" });
   } catch (err) {
-    res.status(400).json({ message: 'Помилка авторизації' });
+    res.status(400).json({ message: "Помилка авторизації" });
   }
 };
 
@@ -119,7 +130,10 @@ const createAdminInDB = async () => {
   const userCount = await User.where({}).countDocuments();
   if (userCount < 1) {
     const hashPassword = bcrypt.hashSync(process.env.START_PASSWORD, 7);
-    const user = new User({ username: process.env.START_LOGIN, password: hashPassword });
+    const user = new User({
+      username: process.env.START_LOGIN,
+      password: hashPassword,
+    });
     await user.save();
   }
 };
