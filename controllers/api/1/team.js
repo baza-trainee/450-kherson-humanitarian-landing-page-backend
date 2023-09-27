0c5a4cf4f13cc39daff785bc5da81e2af3ad8587
Volodymyr Nerovnia <nerv@i.ua>
2023-09-27 16:48:29 +0300

    Improved team controller but it not worked

--- controllers/api/1/team.js
+++ controllers/api/1/team.js
@@ -2,10 +2,19 @@
  * Copyright (c) 2023 Volodymyr Nerovnia
  * SPDX-License-Identifier: MIT
  */
+const TeamDBModel = require("../../../models/api/1/Team");
 
 const getTeam = async (req, res, next) => {
   try {
-    res.status(501).json({ message: "Очікує на реалізацію" });
+    const query = (await TeamDBModel.findOne({}).exec()) ?? {
+      picture: {
+        mime_type: "",
+        image: "",
+      },
+      title: "",
+      text: "",
+    };
+    res.status(200).json({});
   } catch (err) {
     res.status(500).json({ message: "Помилка на боці серверу" });
   }
@@ -13,6 +22,14 @@ const getTeam = async (req, res, next) => {
 
 const updateTeam = async (req, res, next) => {
   try {
+    const team = req.body;
+    const teamToSave = {
+      picture: {
+        mime_type: "text/plain",
+      },
+      title: req.body.title,
+      text: req.body.text,
+    };
     res.status(501).json({ message: "Очікує на реалізацію" });
   } catch (err) {
     res.status(500).json({ message: "Помилка на боці серверу" });
