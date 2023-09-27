7d60c0d5dea9b455d3dbb0f57a9037fe52deca56
Volodymyr Nerovnia <nerv@i.ua>
2023-09-27 16:44:50 +0300

    Added to the imageProcessor a savePicture and a deletePicture functions

--- utils/helpers/api/imageProcessor.js
+++ utils/helpers/api/imageProcessor.js
@@ -7,6 +7,42 @@ const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 
+const { existsSync } = require("node:fs");
+const { Buffer } = require("node:buffer");
+const { writeFile, unlink } = require("node:fs/promises");
+const mimeTypes = require("../../../dictionaries/mime/pictures");
+const appConfig = require("../../../config/app");
+
+const savePicture = async (picture, mime) => {
+  const controller = new AbortController();
+  const { signal } = controller;
+
+  const picFileExtension = mimeTypes.find(
+    (type) => type.mimeName === mime
+  ).extension;
+  const buff = Buffer.from(picture, "base64");
+  const fileName = generateUniqueImageFileName(picFileExtension);
+  await writeFile(
+    path.join(appConfig.publicResources.pictures.directory, `${fileName}`),
+    buff,
+    {
+      signal,
+    }
+  );
+  return fileName;
+};
+
+const deletePicture = async (picture) => {
+  if (existsSync(picture)) {
+    await unlink(picture, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    return true;
+  }
+};
+
 /**
  * Generates a unique image file name
  */
@@ -20,4 +56,6 @@ function generateUniqueImageFileName(extension = "jpg") {
 
 module.exports = {
   generateUniqueImageFileName,
+  savePicture,
+  deletePicture,
 };
