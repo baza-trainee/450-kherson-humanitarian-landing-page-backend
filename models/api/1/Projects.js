fd82e24d043f16b908c5f3b41556d002ee8a3c3a
Oleksandr Pavlishchev <velogitara@gmail.com>
2023-12-30 23:23:15 +0200

    videoLink in file models/Projects set reqired: false

--- models/api/1/Projects.js
+++ models/api/1/Projects.js
@@ -11,16 +11,16 @@ const { Schema, model } = require("mongoose");
 const Picture = require("./Common/Picture");
 
 const Projects = new Schema({
-  // funding-await, in-process, completed
-  stage: { type: String, required: true },
-  videoLink: { type: String, required: true },
-  subTitle: { type: String, required: true },
-  text: { type: String, required: true },
-  areaCompletedWorks: { type: String, required: true },
-  projectDuration: { type: String, required: true },
-  projectText: { type: String, required: true },
-  mainPicture: Picture,
-  pictures: [{ type: Picture }],
+    // funding-await, in-process, completed
+    stage: { type: String, required: true },
+    videoLink: { type: String, required: false },
+    subTitle: { type: String, required: true },
+    text: { type: String, required: true },
+    areaCompletedWorks: { type: String, required: true },
+    projectDuration: { type: String, required: true },
+    projectText: { type: String, required: true },
+    mainPicture: Picture,
+    pictures: [{ type: Picture }],
 });
 
 module.exports = model("Projects", Projects);
